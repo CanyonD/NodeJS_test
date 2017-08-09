88125ae7be357c35f0ee34255dce6681251808c8
Canyon Di <CanyonD8@gmail.com>
2017-08-10 01:30:26 +0300

    Added the test for upload and saving a file to 'tmp_upload' directory. TODO: Need to add redirect to homepage after uploading

--- server.js
+++ server.js
@@ -2,13 +2,39 @@ var http = require("http"),
     url = require("url"),
     path = require("path"),
     fs = require("fs"),
-    port = process.argv[2] || 8080;
+    port = process.argv[2] || 8080,
+    formidable = require('formidable');
 var React = require('react');
 
-http.createServer(function(request, response) {
-
-    var uri = url.parse(request.url).pathname
-        , filename = path.join(process.cwd(), uri);
+http.createServer(function (request, response) {
+    console.log(request.url);
+    var uri = url.parse(request.url).pathname,
+        filename = path.join(process.cwd(), uri);
+    if (request.url === '/test_upload/fileupload') {
+        console.log('start fileuploading...');
+        var form = new formidable.IncomingForm();
+        form.parse(request, function (err, fields, files) {
+            if (typeof(files.filetoupload.path) !== "undefined") {
+                router.post('/test_upload/index.html');
+                // return;
+            }
+            var oldpath = files.filetoupload.path;
+            var newpath = '/opt/web/tmp_upload/' +
+                Math.random()*100000 + '_' + files.filetoupload.name ;
+            fs.rename(oldpath, newpath, function (err) {
+                console.log('saving a file...');
+                if(err) {
+                    response.writeHead(500, {"Content-Type": "text/plain"});
+                    response.write(err + "\n");
+                    response.end();
+                    return;
+                }
+                response.writeHead(200);
+                response.write('File uploaded and moved!');
+                response.end();
+            });
+        });
+    }
 
     fs.exists(filename, function(exists) {
         if(!exists) {
